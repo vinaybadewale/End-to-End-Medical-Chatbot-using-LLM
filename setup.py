22604f76a9a25159f2884b1665b33004a858c83d
Vinay Badewale <vinaybadewale@email.com>
2024-08-07 02:24:46 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+   name='Medical Chatbot',
+   version='0.0.1',
+   author='vinaybadewale',
+   author_email='vinaybadewale@gmail.com',
+   packages = find_packages()
+)
